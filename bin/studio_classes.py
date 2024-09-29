13b578bda6ae79f670bdb7b8e545ad54bf564048
Marwa Naji <mnaji2@jh.edu>
2024-09-29 09:06:08 -0400

    Resolve merge conflicts in biwt_tab.py and studio_classes.py

--- bin/studio_classes.py
+++ bin/studio_classes.py
@@ -2,9 +2,16 @@ import sys
 
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt, QEvent
+<<<<<<< HEAD
 from PyQt5.QtWidgets import QFrame, QCheckBox, QWidget, QLineEdit, QWidget, QComboBox, QLabel, QCompleter, QToolTip, QRadioButton
+=======
+from PyQt5.QtWidgets import QFrame, QCheckBox, QWidget, QLineEdit, QWidget, QComboBox, QLabel, QCompleter, QToolTip, QVBoxLayout, QDialog
+>>>>>>> biwt-local
 from PyQt5.QtGui import QValidator, QDoubleValidator
 from PyQt5.QtCore import Qt, QSortFilterProxyModel, QEvent
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 
 # organizers
 class QHLine(QFrame):
@@ -61,6 +68,25 @@ class QCheckBox_custom(QCheckBox):  # it's insane to have to do this!
                 }
                 """
         self.setStyleSheet(checkbox_style)
+
+class LegendWindow(QDialog):
+    def __init__(self, parent=None, legend_artists=None, legend_labels=None, legend_title=None):
+        super(LegendWindow, self).__init__(parent)
+        self.setWindowTitle(f"Legend: {legend_title}")
+        self.setGeometry(100, 100, 300, 200)
+
+        # Create a figure for the legend
+        self.figure = plt.figure()
+        self.canvas = FigureCanvasQTAgg(self.figure)
+
+        # Create the legend
+        ax = self.figure.add_subplot(111)
+        ax.legend(legend_artists, legend_labels)
+        ax.axis('off')  # Turn off the axis
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
 
 class QLineEdit_custom(QLineEdit):
     def __init__(self, **kwargs):
@@ -220,7 +246,6 @@ class HoverLabel(QLabel, HoverWidget):
         """)
       
 # validators
-
 class DoubleValidatorWidgetBounded(QValidator):
     # a validator that uses other widgets to set the bounds of a QDoubleValidator
     def __init__(self, bottom=None, top=None, bottom_transform=lambda x: x, top_transform=lambda x: x):
